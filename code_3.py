786f609e453d141df751556e220770e14f76d4f4
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2023-12-04 12:33:52 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,56 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
     main()
-    for item in data:
-def main():
     data = generate_random_data()
 
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
+    for item in data:
+    return data
+
+def main():
 
 def generate_random_data():
+import random
+        print(f"Random Number: {item}")    const data = generateRandomData();
+function generateRandomData() {
+
+    }
+    return data;
+main();
+}
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+}
+
+function main() {
+    });
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+if __name__ == "__main__":
+def main():
+    data = generate_random_data()
+    return data
+        print(f"Random Number: {item}")
+def generate_random_data():
+import random
+    for item in data:
+
+
+if __name__ == "__main__":
+    for item in data:
+def main():
 
 
     return data
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+import random
+
+    main()
