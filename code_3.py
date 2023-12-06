8460891ac721f272b7e117b2e9b75ea1d1f2cf2f
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2023-12-06 19:58:34 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,17 +1,27 @@
-    data.forEach(item => {
-    const data = [];
-    for (let i = 0; i < 10; i++) {
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
 
-function main() {
-    const data = generateRandomData();
-}
-    });
-        data.push(Math.floor(Math.random() * 100) + 1);
-    return data;
-    }
-function generateRandomData() {
+    data = generate_random_data()
+    return data
 
-}
+if __name__ == "__main__":
 
-        console.log("Random Number: " + item);
-main();
+    for item in data:
+
+def main():
+        print(f"Random Number: {item}")
+def generate_random_data():
+import random    for item in data:
+
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    return data
+
+    data = generate_random_data()
+import random
+
+if __name__ == "__main__":
