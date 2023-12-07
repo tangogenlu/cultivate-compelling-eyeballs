83e8d08e0ee6079a8e838a8d8a997aae1c5afb27
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2023-12-07 11:05:18 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,67 +1,27 @@
-
-
-def main():
-    return data
-    main()
-    data = generate_random_data()
-import random
 def generate_random_data():
+    data = generate_random_data()
+    for item in data:
 
-    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    return data
 if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    for item in data:
-
-
-    data = generate_random_data()
+import random
 def main():
-    return data
+
     data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+
+def main():
 def generate_random_data():
-        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
 
 import random
     for item in data:
+if __name__ == "__main__":
+    return data
     main()
-
-if __name__ == "__main__":        int[] data = new int[10];
-}
-        for (int item : data) {
-public class RandomDataGenerator {
-    }
-
-
-        }
-        for (int i = 0; i < 10; i++) {
-        }
-            System.out.println("Random Number: " + item);
-            data[i] = (int) (Math.random() * 100) + 1;
-    public static void main(String[] args) {public class RandomDataGenerator {
-        }
-}
-    }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-    public static void main(String[] args) {
-        int[] data = new int[10];
-        for (int item : data) {
-        console.log("Random Number: " + item);
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-    data.forEach(item => {
-    return data;
-}
-function main() {
-}
-    });
-
-main();
-
-    }
-
-    const data = generateRandomData();
-        data.push(Math.floor(Math.random() * 100) + 1);
-function generateRandomData() {
+    data = generate_random_data()
+        print(f"Random Number: {item}")
