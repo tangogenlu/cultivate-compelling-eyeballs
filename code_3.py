3e0cdefc9eb6bd19c4e08725256b227b4d96c24b
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2023-12-10 14:38:41 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,30 +1,17 @@
-    return data
-
-        print(f"Random Number: {item}")
-    data = generate_random_data()
-def main():
-
-    main()
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-def generate_random_data():
-
-if __name__ == "__main__":
-import random    });
-
-main();
-function main() {
-
-
     const data = generateRandomData();
-    for (let i = 0; i < 10; i++) {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    return data;
-        console.log("Random Number: " + item);
-}
-    data.forEach(item => {
-function generateRandomData() {
-}
-    const data = [];
+    });
+
     }
+}
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+function main() {
+    for (let i = 0; i < 10; i++) {
+}
+main();
+function generateRandomData() {
+    return data;
+
+    const data = [];
+        console.log("Random Number: " + item);
